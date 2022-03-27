51d192d8af5895da65aaae9fc084f1bec2784f18
zuhri <zuhrywijianto@gmail.com>
2022-03-27 13:52:40 +0700

    Dashboard Layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,71 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+  <v-row align="center">
+    <v-item-group
+      v-model="window"
+      class="shrink mr-6"
+      mandatory
+      tag="v-flex"
+    >
+      <v-item
+        v-for="n in length"
+        :key="n"
+        v-slot="{ active, toggle }"
+      >
+        <div>
           <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
+            :input-value="active"
+            icon
+            @click="toggle"
           >
-            Continue
+            <v-icon>mdi-record</v-icon>
           </v-btn>
-        </v-card-actions>
-      </v-card>
+        </div>
+      </v-item>
+    </v-item-group>
+
+    <v-col>
+      <v-window
+        v-model="window"
+        class="elevation-1"
+        vertical
+      >
+        <v-window-item
+          v-for="n in length"
+          :key="n"
+        >
+          <v-card flat>
+            <v-card-text>
+              <v-row
+                class="mb-4"
+                align="center"
+              >
+                <v-avatar
+                <PemkabLogo />
+                </v-avatar>
+                <strong class="text-h6">Menuju Zona Integritas</strong>
+                <v-spacer></v-spacer>
+              </v-row>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+      </v-window>
     </v-col>
   </v-row>
 </template>
-
 <script>
-export default {
-  name: 'IndexPage'
-}
+  export default {
+    data: () => ({
+      length: 3,
+      window: 0,
+    }),
+  }
 </script>
